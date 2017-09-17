fce514650b5e73f646b38a215705eac4f02cb6a1
rock4you <rockpeng@foxmail.com>
2017-09-17 10:28:04 +0800

    POJ-1007-DNA Sorting
    http://poj.org/problem?id=1007

---
+++ POJ-1007-DNA Sorting.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+int main() {
+    int n;//len of DNA
+    int m;//num of DNA
+    int i, j, k, t, mini, order;
+    struct mychar{
+        char str[50];
+        int initorder;
+        int inversion;
+    };
+    struct mychar DNA[100];
+    scanf("%d %d", &n, &m);
+
+    for (i=0;i<m;i++)
+    {
+        DNA[i].initorder=i+1;
+        scanf("%s", &DNA[i].str);
+        t=0;
+
+        for(j=0;j<n;j++)
+        {
+            for(k=j+1;k<n;k++)
+            {
+                if(DNA[i].str[j]>DNA[i].str[k])
+                {
+                    t+=1;
+                }
+            }
+        }
+        DNA[i].inversion=t;
+        //printf("inver=%d\n", DNA[i].inversion);
+    }
+    //printf("\n\n");
+
+
+
+    for(i=0;i<m;i++)
+    {
+        mini=4951;
+        for(j=0;j<m;j++)
+        {
+
+            if(DNA[j].inversion<mini)
+            {
+                mini=DNA[j].inversion;
+                order=j;
+            }
+        }
+        //printf("mini is:%d\n", mini);
+        printf("%s\n",DNA[order].str);
+        DNA[order].inversion=4951;
+    }
+    return 0;
+}
+
+/*
+10 6
+AACATGAAGG
+TTTTGGCCAA
+TTTGGCCAAA
+GATCAGATTT
+CCCGGGGGGA
+ATCGATGCAT
+*/
