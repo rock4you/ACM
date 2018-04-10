97048d1ab82f605b6df4d53f432293fe8049bd99
rock4you <rockpeng@foxmail.com>
2018-04-10 21:02:22 +0800

    素因子分解

--- prime.c
+++ prime.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-//素因子分解 naive版本 仅能应对2^41以下的整数
-
+//素因子分解 naive版本 仅能应对2^63以下的整数
+typedef unsigned __int64 xint;
 
 int main()
 {
-    __int64 a, i, k=0, num[200], cishu[200], index=0;
-    a=(__int64)pow(2,41)-1;
+    xint a, i, k=0, num[200], cishu[200], index=0;
+    a=(xint)pow(2,63)-1;
 
     memset(num, 0, sizeof(num));///这里应该用sizeof而不是200.是按照字节算的
     memset(cishu, 0, sizeof(cishu));
-
-    for(i=2; (a>1)&&(i<(__int64)sqrt(a)+1); i++)
+    printf("%I64d=",a);
+    for(i=2; (a>1)&&(i<(xint)sqrt(a)+1); i++)
     {
         if(a%i==0)
         {
@@ -29,11 +29,11 @@ int main()
             k++;
         }
     }
-    printf("%I64d=",a);
+
     for(i=0; i<200; i++)
     {
         if(num[i]!=0)
-            printf("%I64d(%I64d)", num[i], cishu[i]);
+            printf("%I64u(%I64u)", num[i], cishu[i]);
         if(index>1)
         {
             printf("*");
@@ -44,7 +44,7 @@ int main()
     {
         if(index==1)
             printf("*");
-        printf("%I64d(%d)", a, 1);
+        printf("%I64u(%d)", a, 1);
     }
     return 0;
 }
